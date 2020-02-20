74501ce993cf0ffe09ea946c95075ccb674dbbb9
Felipe <felipe.seraf@gmail.comgit config --global user.name Felipegit coomit -m Firstgit config --global user.email felipe.seraf@gmail.com>
2020-02-20 15:53:41 -0300

    Updates

---
+++ precache-manifest.8060c7bdc72688729bc8a0d3ca42ca8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e4ff3032fb795559fee429adbf6838",
+    "url": "/cj-ipsum/index.html"
+  },
+  {
+    "revision": "3e4652f68f5817673f31",
+    "url": "/cj-ipsum/static/css/2.5bc4965f.chunk.css"
+  },
+  {
+    "revision": "498134b07c73abb9d4a6",
+    "url": "/cj-ipsum/static/css/main.1823c4b7.chunk.css"
+  },
+  {
+    "revision": "3e4652f68f5817673f31",
+    "url": "/cj-ipsum/static/js/2.82d794a9.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/cj-ipsum/static/js/2.82d794a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "498134b07c73abb9d4a6",
+    "url": "/cj-ipsum/static/js/main.6487a596.chunk.js"
+  },
+  {
+    "revision": "e4c17128ee1d7503df36",
+    "url": "/cj-ipsum/static/js/runtime-main.6af76b5a.js"
+  }
+]);
